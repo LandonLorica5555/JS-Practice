7cb730781466f46ef9437aefa358f221262ed574
Landon <landonlorica41@gmail.com>
2025-09-16 01:24:56 +0800

    lesson 11 part 1

--- Lesson 10 - HTML, CSS, and JavaScript together/scripts/10d.js
+++ Lesson 10 - HTML, CSS, and JavaScript together/scripts/10d.js
@@ -1,19 +1,16 @@
-function buttonClick(selector) {
+function toggleButton(selector) {
   const button = document.querySelector(selector);
+
   if (!button.classList.contains("is-toggled")) {
-
-     // Before turning this button ON, check if there's
-    // already a button that's turned ON and turn it OFF.
-    turnOffPreviousButton();
-
+    checkPreviousButton();
     button.classList.add("is-toggled");
   } else {
     button.classList.remove("is-toggled");
   }
 }
 
-function turnOffPreviousButton() {
-    const previousButton = document.querySelector('.is-toggled');
+function checkPreviousButton() {
+    const previousButton = document.querySelector('.is-toggled')
 
     if (previousButton) {
         previousButton.classList.remove('is-toggled');
